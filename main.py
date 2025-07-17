83fc30570931bf3adf8d15ec39c2b610f12c08ed
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-07-17 20:52:04 +0100

    feat: integrate customer registration into main application flow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from models.bike import Bike
+from models.customer import Customer
 from services.bike_service import add_bike, generate_bike_id
 from datetime import date
+
+from services.customer_service import add_customer, generate_customer_id
 
 print("+","="*37,"+")
 print("| WELCOME TO THE APP BIKE RENTAL SYSTEM |")
@@ -28,8 +31,8 @@ while True:
     # elif choice == "1": vou fazer essa parte em um segundo momento
     elif choice =="2":
         bike_id = generate_bike_id()
-        model = input("Enter the bike model: ").strip().title()
-        bike_type = input("Enter the bike type: ").strip().title()
+        model = input("Enter the bike model (e.g. Trek FX or Caloi 29): ").strip().title()
+        bike_type = input("Enter the bike type (e.g. Urban, Montain or Eletric): ").strip().title()
         is_available = True
         registered_date = date.today().isoformat()
 
@@ -42,3 +45,29 @@ while True:
 
         add_bike(bike_obj)
         print("\nBike registered successfully!\n")
+
+    elif choice =="3":
+        customer_id = generate_customer_id()
+        name = input("Enter with customer name (e.g. Luke Skywalker.): ").strip().title()
+
+        while True:
+            email = input("Enter email: (e.g. luke_skywalker@deathstar.com.): ").strip().lower()
+            if "@" in email and "." in email:
+                break
+            else:
+                print("\nInvalid email format. Please try again.\n")
+
+        phone = input("Enter phone number (e.g. 089360-5640): ").replace("-", "").replace(" ","")
+        registered_date = date.today().isoformat()
+
+        customer_obj = Customer(customer_id,
+                                name,
+                                email,
+                                phone,
+                                registered_date
+        )
+
+        add_customer(customer_obj)
+        print("\nCustomer registered successfully!\n")
+
+
