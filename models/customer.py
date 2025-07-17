43e728c17f6830edb320d9ab656e07fdd6f250a6
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-07-17 20:50:42 +0100

    feat: create Customer model with attributes and date registration logic

---
+++ models/customer.py
@@ -0,0 +1,20 @@
+class Customer:
+    def __init__(self, customer_id,
+                 name,
+                 email,
+                 phone,
+                 registered_date):
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.registered_date = registered_date
+
+    def to_dict(self):
+        return {
+            "customer_id": self.customer_id,
+            "name": self.name,
+            "email": self.email,
+            "phone": self.phone,
+            "registered_date": self.registered_date
+        }
