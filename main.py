c00ba43445df78d3db24dce5a89b31e73289cdde
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-07-27 20:23:52 +0100

    feat: add menu option to search bike by ID or type

--- main.py
+++ main.py
@@ -2,17 +2,21 @@
 This module provides the main user interface for the App Bike Rental System,
 allowing interaction through a command-line menu.
 """
+from re import search
 
-from services.bike_service import register_new_bike
+from services.bike_service import register_new_bike, display_all_bikes, search_bike
 from services.customer_service import register_new_customer
 from services.rental_service import register_new_rent, register_bike_return
 
 print("+","="*37,"+")
 print("| WELCOME TO THE APP BIKE RENTAL SYSTEM |")
-print("+","="*37,"+\n")
+print("+","="*37,"+")
 
 while True:
+    print("\n")
     print("+","="*37, "+")
+    print("|"," "*15,"HOME", " "*16,"|")
+    print("+", "="*37,"+")
     print("| 1 - Add new bike", " "*20,"|")
     print("| 2 - Register customer", " "*15,"|")
     print("| 3 - Rent a bike", " "*21,"|")
@@ -47,4 +51,9 @@ while True:
     elif choice == "4":
         register_bike_return()
 
+    elif choice == "5":
+        display_all_bikes()
+
+    elif choice =="6":
+        search_bike()
 
